1102a736b5bc33ac318970bf53596dca23d671f9
Samuel Gélineau <gelisam+github@gmail.com>
2017-09-29 13:37:19 -0400

    rename 'apply' to 'patch'
    So we now have 'diff' and 'patch', which matches the terminology of
    the shell programs with the same names, and avoids confusion with
    "applying" a function.

--- src/Island/Diff.hs
+++ src/Island/Diff.hs
@@ -23,34 +23,34 @@ import qualified Generics.Eot as Eot
 --
 -- Expressed as laws:
 --
--- > diff x y `apply` x = y
--- > apply mempty = id
--- > apply (p <> q) = apply p >>> apply q
+-- > patch (diff x y) x = y
+-- > patch mempty = id
+-- > patch (p <> q) = patch p >>> patch q
 class Monoid (Patch a) => Diff a where
   type Patch a
 
   diff  :: a -> a -> Patch a
-  apply :: Patch a -> a -> a
+  patch :: Patch a -> a -> a
 
   default diff :: (HasEot a, Diff (Eot a))
                => a -> a -> Patch (Eot a)
   diff = genericDiff
 
-  default apply :: (HasEot a, Diff (Eot a))
+  default patch :: (HasEot a, Diff (Eot a))
                 => Patch (Eot a) -> a -> a
-  apply = genericApply
+  patch = genericPatch
 
 instance Diff Void where
   type Patch Void = ()
 
   diff _ _ = ()
-  apply () = id
+  patch () = id
 
 instance Diff () where
   type Patch () = ()
 
   diff _ _ = ()
-  apply () = id
+  patch () = id
 
 
 -- * Atomic types
@@ -71,9 +71,9 @@ atomicDiff :: Eq a => a -> a -> Last a
 atomicDiff a1 a2 | a1 == a2  = Last Nothing
                  | otherwise = Last . Just $ a2
 
-atomicApply :: Last a -> a -> a
-atomicApply (Last (Just a))_ = a
-atomicApply (Last Nothing) a = a
+atomicPatch :: Last a -> a -> a
+atomicPatch (Last (Just a))_ = a
+atomicPatch (Last Nothing) a = a
 
 
 -- | A newtype wrapper which gives an atomic 'Diff' instance to any 'Eq'.
@@ -85,20 +85,20 @@ instance Eq a => Diff (Atomic a) where
   type Patch (Atomic a) = Last a
 
   diff (Atomic a1) (Atomic a2) = atomicDiff a1 a2
-  apply p = Atomic . atomicApply p . unAtomic
+  patch p = Atomic . atomicPatch p . unAtomic
 
 
 instance Diff Int where
   type Patch Int = Patch (Atomic Int)
 
   diff = atomicDiff
-  apply = atomicApply
+  patch = atomicPatch
 
 instance Diff Text where
   type Patch Text = Patch (Atomic Text)
 
   diff = atomicDiff
-  apply = atomicApply
+  patch = atomicPatch
 
 
 -- * Algebraic data types
@@ -122,15 +122,15 @@ instance Diff Eot.Void where
   type Patch Eot.Void = ()
 
   diff _ _ = ()
-  apply () = id
+  patch () = id
 
 genericDiff :: (HasEot a, Diff (Eot a))
             => a -> a -> Patch (Eot a)
 genericDiff x y = diff (toEot x) (toEot y)
 
-genericApply :: (HasEot a, Diff (Eot a))
+genericPatch :: (HasEot a, Diff (Eot a))
              => Patch (Eot a) -> a -> a
-genericApply p = fromEot . apply p . toEot
+genericPatch p = fromEot . patch p . toEot
 
 
 instance Diff Bool where
@@ -143,18 +143,18 @@ instance (Diff a, Diff b) => Diff (a, b) where
   type Patch (a, b) = (Patch a, Patch b)
 
   diff (a1, b1) (a2, b2) = (diff a1 a2, diff b1 b2)
-  apply (a12, b12) (a1, b1) = (apply a12 a1, apply b12 b1)
+  patch (a12, b12) (a1, b1) = (patch a12 a1, patch b12 b1)
 
 
 -- * Sum types
 
 -- $
--- Sum types are the main reason updates can fail: if we have a 'Patch' which expects a @Left a@ but we 'apply' it to a
+-- Sum types are the main reason updates can fail: if we have a 'Patch' which expects a @Left a@ but we 'patch' it to a
 -- @Right b@, we have no choice but to fail with an 'UnexpectedRight' error. Another possibility is a 'Patch' which
 -- switches from one constructor to another, 'LeftToRight' for example. In this case, the values are so unrelated that
 -- we have no choice but to store the entire new value, and also the old value so we can go in the other direction.
 -- Since we have an entire replacement value we could use as the answer, we again have the choice to succeed or to fail
--- if we apply such a constructor-swapping 'Patch' to a value which differs from the old value we recored. In this
+-- if we patch such a constructor-swapping 'Patch' to a value which differs from the old value we recored. In this
 -- implementation, for consistency with the atomic case, we fail with a 'MismatchedLeft' if that occurs.
 
 data PatchEither a b
@@ -165,8 +165,8 @@ deriving instance (Show a, Show b, Show (Patch a), Show (Patch b)) => Show (Patc
 instance (Diff a, Diff b) => Monoid (PatchEither a b) where
   mempty = PatchEither mempty mempty
   _                        `mappend` p@ReplaceEither {}  = p
-  ReplaceEither (Left  a2) `mappend` PatchEither a23 _   = ReplaceEither . Left  . apply a23 $ a2
-  ReplaceEither (Right b2) `mappend` PatchEither _   b23 = ReplaceEither . Right . apply b23 $ b2
+  ReplaceEither (Left  a2) `mappend` PatchEither a23 _   = ReplaceEither . Left  . patch a23 $ a2
+  ReplaceEither (Right b2) `mappend` PatchEither _   b23 = ReplaceEither . Right . patch b23 $ b2
   PatchEither a12 b12      `mappend` PatchEither a23 b23 = PatchEither (a12 <> a23) (b12 <> b23)
 
 instance (Diff a, Diff b) => Diff (Either a b) where
@@ -176,6 +176,6 @@ instance (Diff a, Diff b) => Diff (Either a b) where
   diff (Right b1) (Right b2) = PatchEither mempty (diff b1 b2)
   diff _          x          = ReplaceEither x
 
-  apply (ReplaceEither x)   _          = x
-  apply (PatchEither a12 _) (Left  a1) = Left  . apply a12 $ a1
-  apply (PatchEither _ b12) (Right b1) = Right . apply b12 $ b1
+  patch (ReplaceEither x)   _          = x
+  patch (PatchEither a12 _) (Left  a1) = Left  . patch a12 $ a1
+  patch (PatchEither _ b12) (Right b1) = Right . patch b12 $ b1
